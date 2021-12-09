41ab36eacb4e9b7fd6906ea6fef2f91f818ee20e
xennia316 <xennia316@gmail.com>
2021-12-09 09:42:13 +0100

    Implementation of the about page

---
+++ happy_child/src/About.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function About ()
+{
+    return(
+    <div className="about_us">
+        <header>
+            <h1> About Us</h1>
+            <h1> Our mission</h1>
+        </header>
+        <section>
+            
+            <p>
+                <b>Every child deserves to learn!</b>
+                <br/>
+                <br/>
+                It's rather unfurtunate that the world we live in has resolved to making money out of the ribs of little kids rather actually helping them.
+                Numerous organizations out there claiming to better the lives if people but end up squandering the support they get in reckless ways .
+                <br/>
+                <br/>
+                We thought it wise to build a platform for those who want to help children but have not found a reliable or trustworthy means yet.
+                <br/>
+                We'll not need you to pass through us. Hence, eliminating ambiguity. 
+                We only serve as a link between you and a happy child. 
+                <br/>
+            </p>
+        </section>
+        <div className="goals">
+            <p>
+                We aim at bringing joy to atleast one child everyday.
+            </p>
+
+        </div>
+    </div>
+    );
+}
+
+export default About;
--- happy_child/src/App.js
+++ happy_child/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import About from './About';
+
 
 function App() {
   return (
     <div className="App">
+      <About/>
       
     </div>
   );
