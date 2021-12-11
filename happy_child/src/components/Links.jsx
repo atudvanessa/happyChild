fb502a555f241ab834e6d9d90d544591d526f5ab
giftcup <salometambe@gmail.com>
2021-12-11 22:58:44 +0100

    Implement responsiveness on the navbar

---
+++ happy_child/src/components/Links.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Links() {
+    return (
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/helper">Helper</Link>
+          <Link to="/donor">Donor</Link>
+        </div>
+    );
+}
+export default Links;
--- happy_child/src/components/navbar.jsx
+++ happy_child/src/components/navbar.jsx
@@ -1,36 +1,46 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import Logo from "../assets/logo.png";
+import Links from "./Links";
 
 function NavBar() {
   const [small, setSmall] = useState(null);
-  //#endregion
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const menu = () => {
+    setMenuIsOpen(!menuIsOpen);
+  };
+
   const handleResize = () => {
     console.log(window.innerWidth);
-
     window.innerWidth < 800 ? setSmall(true) : setSmall(false);
   };
+
   useEffect(() => {
     handleResize();
     window.addEventListener("resize", handleResize);
   }, []);
+
   return (
-    <div className="navigation">
-      <div className="image">
-        <img src={Logo} alt="logo" />
+    <div>
+      <div className="navigation">
+        <div className="image">
+          <img src={Logo} alt="logo" />
+        </div>
+        {small === false ? (
+          <Links />
+        ) : (
+          <div className="menu">
+            <i className="fa fa-bars fa-2x" aria-hidden="true" onClick={menu} />
+          </div>
+        )}
       </div>
-      {small === false ? (
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/helper">Helper</Link>
-          <Link to="/donor">Donor</Link>
+
+      {menuIsOpen && small ? (
+        <div className="small_menu">
+          <Links />{" "}
         </div>
       ) : (
-        <div className="menu">
-          <i class="fa fa-bars fa-2x" aria-hidden="true"></i>
-        </div>
+        <div></div>
       )}
     </div>
   );
